6de69019d7ab6db11f393ecf9ab27cb5ee92665a
Alexander Baker <alex.baker685@gmail.com>
2019-03-04 22:52:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,4 +13,17 @@ binding.pry
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # Location: project.css("ul.project-meta li a span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first funded strong").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+ 
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+   projects[project] = {}
+  end
+
+projects  
+end
