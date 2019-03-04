26024ce7778c8d87538bf796ab3790022865c3cd
Alexander Baker <alex.baker685@gmail.com>
2019-03-04 22:56:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,13 +1,6 @@
 require 'pry'
 require 'nokogiri'
 
-html = File.read('fixtures/kickstarter.html')
-
-kickstarter = Nokogiri::HTML(html)
-
-project = kickstarter.css("li.project.grid_4").first
-binding.pry
-
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
